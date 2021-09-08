9325bcb14e9cf375abccefc60c7d649466420a2c
陈泽 <chenze_ustc@mail.ustc.edu.cn>
2021-09-08 17:02:18 +0800

    Default commander recipe

---
+++ src/commander-recipe.ts
@@ -0,0 +1,21 @@
+import { Recipe } from './recipe';
+
+export interface CommanderRecipe extends Recipe {
+    type: 'Commander';
+}
+
+export function isCommanderRecipe(entity: Recipe): entity is CommanderRecipe {
+    return entity.type === 'Commander';
+}
+
+const DEFAULT_COMMANDER_RECIPE: CommanderRecipe = {
+    type: 'Commander',
+    privateVariables: [],
+    publicVariables: [],
+    privateExports: [],
+    publicExports: [],
+}
+
+export function defaultCommanderRecipe(): CommanderRecipe {
+    return DEFAULT_COMMANDER_RECIPE;
+}
