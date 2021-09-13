d0ccd86dfe26c117652eaae83820c7242aaf734d
陈泽 <chenze_ustc@mail.ustc.edu.cn>
2021-09-13 17:06:11 +0800

    1.0.20

--- src/__tests__/keithley-simple.test.ts
+++ src/__tests__/keithley-simple.test.ts
@@ -4,6 +4,6 @@ import * as keithley2600 from '../keithley-2636-simple-recipe';
 test('Keithley 2636 Simple', () => {
     expect(
         smuRecipe.isOffChannelRecipe(keithley2600.defaultKeithley2636SimpleRecipe().smuARecipe) &&
-        smuRecipe.isOffChannelRecipe(keithley2600.defaultKeithley2636SimpleRecipe().smuBRecipe),
+            smuRecipe.isOffChannelRecipe(keithley2600.defaultKeithley2636SimpleRecipe().smuBRecipe),
     ).toBe(true);
 });
