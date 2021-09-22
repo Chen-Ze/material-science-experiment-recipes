f645c4e25abb729b5cfd47b8a6f3d53914d85098
陈泽 <chenze_ustc@mail.ustc.edu.cn>
2021-09-22 15:19:19 +0800

    LightField

---
+++ src/lightfield.ts
@@ -0,0 +1,37 @@
+import { AvailableVariables, Recipe } from './recipe';
+
+export enum LightFieldTask {
+    ActivateWindow = "Activate Window",
+    SaveSpectrum = "Save Spectrum"
+}
+
+export interface LightFieldRecipe extends Recipe {
+    type: 'LightField';
+    task: LightFieldTask;
+    payload: {
+        [key: string]: string
+    }
+}
+
+export function isLightFieldRecipe(entity: Recipe): entity is LightFieldRecipe {
+    return entity.type === 'LightField';
+}
+
+export function defaultLightFieldRecipe(): LightFieldRecipe {
+    return {
+        type: 'LightField',
+        task: LightFieldTask.ActivateWindow,
+        payload: {},
+        privateVariables: [],
+        publicVariables: [],
+        privateExports: [],
+        publicExports: [],
+    };
+}
+
+export function getLightFieldRecipeVariables(recipe: LightFieldRecipe): AvailableVariables {
+    return {
+        private: [],
+        public: [],
+    };
+}
